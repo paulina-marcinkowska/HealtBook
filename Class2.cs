3ff4c42fc2830f295dc41334d3540d3b8fa6ea0c
Paulina Marcinkowska <108899098+paulina-marcinkowska@users.noreply.github.com>
2022-08-18 15:51:56 +0200

    Move model to class library (part1

---
+++ Class2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ClassLibrary
+{
+    [XmlRoot(ElementName = "clinic")]
+    public class Clinic
+    {
+        [XmlElement(ElementName = "title")]
+        public string Name { get; set; }
+        [XmlElement(ElementName = "address")]
+        public string Address { get; set; }
+        [XmlElement(ElementName = "contact")]
+        public List<string> Contact { get; set; }
+        [XmlElement(ElementName = "service")]
+        public List<string> Service { get; set; }
+        [XmlElement(ElementName = "doctors")]
+        public List<string> Doctors { get; set; }
+
+        public Clinic(string name, string address)
+        {
+            this.Name = name;
+            this.Address = address;
+        }
+
+        public Clinic()
+        {
+        }
+        public override string ToString()
+        {
+            string currentData = "Nazwa: " + Name + "   " + "Nazwa: " + Address + "   " + "Kontakt: ";
+            foreach (string contact in Contact)
+            {
+                currentData += string.Format("{0}   ", contact);
+            }
+            return currentData;
+        }
+        public void EditClinicData()
+        {
+            try
+            {
+                Console.WriteLine("Podaj adres: ");
+                Address = Console.ReadLine();
+                Console.WriteLine("Dodaj nowy kontakt: ");
+                string newContact = Console.ReadLine();
+                Contact.Add(newContact);
+                Console.WriteLine("Dodaj nowe usługi: ");
+                string newService = Console.ReadLine();
+                Service.Add(newService);
+                Console.WriteLine("Dodaj nowego lekarza: ");
+                string newDoctor = Console.ReadLine();
+                Doctors.Add(newDoctor);
+            }
+
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                EditClinicData();
+            }
+        }
+    }
+}
---
+++ Visit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ClassLibrary
+{
+    [XmlRoot(ElementName = "visit")]
+    public class Visit:Clinic
+    {
+        [XmlRoot(ElementName = "visit")]
+
+            [XmlElement(ElementName = "dateOfVisit")]
+            public DateTime DateOfVisit { get; set; }
+            [XmlElement(ElementName = "doctor")]
+            public string Doctor { get; set; }
+            [XmlElement(ElementName = "diagnosis")]
+            public string Diagnosis { get; set; }
+            [XmlElement(ElementName = "recommendation")]
+            public string Recommendation { get; set; }
+
+            public Visit(string name, string address, string doctor, DateTime dateOfVisit) : base(name, address)
+            {
+                this.Doctor = doctor;
+                this.DateOfVisit = dateOfVisit;
+            }
+
+            public Visit() : base()
+            {
+            }
+        }
+}
